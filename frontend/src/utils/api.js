dd758b76472fce8ca2ea5dd039beeb6d1d0f37e0
Anisha354 <Aangrish9668@conestogac.on.ca>
2025-08-05 20:49:56 -0400

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (process.env.REACT_APP_API_URL || "http://localhost:8000").replace(
+  baseURL: (process.env.REACT_APP_API_URL || "https://casptone-project-group-3-backend.onrender.com").replace(
     /\/+$/,
     ""
   ),
